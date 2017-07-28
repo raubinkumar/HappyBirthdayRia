7e09c84ed2205339702880839b5822c934ba9f84
Raubin Kumar <raubkuma@in.ibm.com>
2017-07-28 18:17:15 +0000

    Minor fixes to css and message

--- Handler.js
+++ Handler.js
@@ -3,12 +3,12 @@
 $(document).ready(function(){
 	// Varibales
     var messageText = [];
-	messageText[0] ="There is a reason why I call you beauty overdosed. Though I am not going to share this here but those who are seeing this picture must be agreeing with me.";
-	messageText[1] = "You are special and you were (are) cute. With this, I remember your words <b>Girls are made to be loved</b>. Though it came up after a silly fight but I agree with you and I'll say, I Love you so much.";
-	messageText[2] = "Its your birthday. Now you have more grown up, every year you are becoming better but you were and are always perfect for me.";
-	messageText[3] = "This is the 5th year we are sharing and growing together, this is like a treasure to me. I can remember our every moment of laughter, love, care and last but not the least The great Debates which I never won. ";
-	messageText[4] = "This is the picture which is like the most and it is close to my heart. You were sick but still you were so beautiful.";
-	messageText[5] = "<bf>Happy Birthday Cintaku, Sayangku, Kashiku.</bf>";
+	messageText[0] ="There is a reason why I call you beauty overdosed. Though I am not going to share this here, but those who are seeing this picture must be agreeing with me.";
+	messageText[1] = "You are special and you were (are) cute. With this, I remember your words <bf> Girls are made to be loved </bf>. Though it came up after a silly fight, but I agree with you and I'll say, I Love you so much.";
+	messageText[2] = "It's your birthday. Now you have more grown up, every year you are becoming better, but you were and are always perfect to me.";
+	messageText[3] = "This is the 5th year we are sharing and growing together, this is like a treasure to me. I can remember our every moment of laughter, love, care and last but not the least The Great Debates, which I never won.";
+	messageText[4] = "This is the picture which I like the most and it is close to my heart. You were sick, but still you were so beautiful.";
+	messageText[5] = "<bf>Happy Birthday Cintaku, Sayangku, Kasihku.</bf>";
 	var windowWidth = window.innerWidth;
 	var windowHeight = window.innerHeight;
 	var timerTime = 20;
